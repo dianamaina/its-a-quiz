335606cbf91ee8f9acc84154cd03663d6e8e9182
diana maina <dianamainachui@gmail.com>
2020-10-06 12:48:34 -0400

    added assets fuile

---
+++ assets.js/questions.js
@@ -0,0 +1,36 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["string", "boolean", "alerts", "numbers"],
+        answers: "alerts",
+    },
+
+
+
+    {title: "The condtion in an if/else statement is encloded within _:",
+    choices: ["quotes", "curly brackets", "paretheses", "square brackets"],
+    answers: "parentheses",
+},
+
+
+   
+    {
+        title: "Arrat is javaScript can  be used to store_:",
+    choices: ["numbers and strings", "other arrays", "booleans", "all the above"],
+    answers: "all the above.",
+},
+
+ {
+        title: "String value must be enclosed within _ when being assigned to variables.",
+    choices: ["commas", "curlty brackets", "quotes", "parenthese"],
+    answers: "quotes",
+},
+
+
+
+ {
+    title: "A very useful tool used during developement and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "ternminal/bash", "for loops", "console.log"],
+    answers: "console.log",
+},
+]
---
+++ assets.js/script.js
@@ -0,0 +1,98 @@
+document.addEventListener("click",function(event){
+    console.log(position)
+        nextQuestion(); 
+    })
+    
+    function nextQuestion(){
+        position++
+        document.querySelector("#question_header").innerText = "<h4>"+questionArray[position].question+"</h4>"
+    
+    }
+
+    function timer (){
+        time = 60
+        if(time i=0, ){
+            stopInterval(){}
+            stopQuiz(){
+                console.log("you need to code out how to stop the quiz")
+            }
+        }
+       }
+       
+startInterval(timer,1000)
+
+stopInterval(timer, 1000)
+
+
+
+// timer
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+
+
+function startQuiz() {
+  
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+
+ 
+  getQuestion();
+}
+
+function getQuestion() {
+  
+  var currentQuestion = questions[currentQuestionIndex];
+
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+  choicesEl.innerHTML = "";
+
+
+  currentQuestion.choices.forEach(function(choice, i) {
+    
+    var choicebutton = document.createElement("button");
+    choicebutton.setAttribute("class", "choice");
+    choicebutton.setAttribute("value", choice);
+
+    choicebutton.textContent = i + 1 + ". " + choice;
+    choicebutton.onclick = questionClick;
+    choicesEl.appendChild(choicebutton);
+  });
+}
+
+
+function saveHighscore() {
+  var initials = initialsEl.value.trim();
+
+  if (initials !== "") {
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    
+    window.location.href = "highscores.html";
+  }
+}
+
+
+
+
+submitBtn.onclick = saveHighscore
+startBtn.onclick = startQuiz;
+initialsEl.onclick = enterinitials;
